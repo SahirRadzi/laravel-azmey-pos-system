3e02a27fab5c839269c60f773b56705eb875b2e5
SahirRadzi <sahir.radzi@gmail.com>
2024-12-28 23:57:34 +0800

    add sidebarCollapsibleOnDesktop

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,6 +33,7 @@ class AdminPanelProvider extends PanelProvider
             ->font(family:'Poppins') // font family stye 'Poppins' / By Default 'Inter'
             // ->darkMode(condition: false) // darkMode setting boolean
 
+            ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters')
